f1306d4c28fdd8fbbe544ab69fa8e7330d76104a
joil <joil-z@outlook.com>
2024-04-03 17:57:01 +0800

    feat: 实现本地滑动窗口限流器

---
+++ ratelimit/local_slide_window.go
@@ -0,0 +1,21 @@
+package ratelimit
+
+import (
+	"time"
+
+	"github.com/udugong/limiter/internal/ratelimit"
+)
+
+// NewLocalSlideWindowLimiter 创建一个本地滑动窗口限流器.
+// window 窗口大小
+// boundedQueue 有界队列
+// 表示: 在 window 内允许有界队列大小的请求
+func NewLocalSlideWindowLimiter(window time.Duration, boundedQueue ratelimit.BoundedQueue,
+	opts ...ratelimit.Option) *ratelimit.LocalSlideWindowLimiter {
+	return ratelimit.NewLocalSlideWindowLimiter(window, boundedQueue)
+}
+
+// WithTimeFunc 控制时间.
+func WithTimeFunc(fn func() time.Time) ratelimit.Option {
+	return ratelimit.WithTimeFunc(fn)
+}
